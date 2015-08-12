913e17066c5e6931b1fb6a3f173045e00fc204a2
Michael Gillett <mgillett54@gmail.com>
2015-08-11 23:48:05 -0400

    Added a fade example

---
+++ firmware/examples/fade_example.ino
@@ -0,0 +1,11 @@
+#include "rgb-controls/rgb-controls.h"
+
+using namespace RGBControls;
+
+Led led(D0, D1, D2);
+Color red(255, 0, 0);
+Color blue(255, 0, 0);
+
+void loop() {
+  led.fade(red, blue);
+}
